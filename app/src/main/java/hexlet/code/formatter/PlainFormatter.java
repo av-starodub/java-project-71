6bd3d5da1506f405eb575f0e59827290830a77cd
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-01-14 02:30:17 +0300

    Implement PlainFormatter class.

---
+++ app/src/main/java/hexlet/code/formatter/PlainFormatter.java
@@ -0,0 +1,43 @@
+package hexlet.code.formatter;
+
+import hexlet.code.property.Property;
+
+import java.util.List;
+import java.util.Map;
+
+public final class PlainFormatter implements Formatter {
+    @Override
+    public StringBuilder format(StringBuilder sb, Property prop) {
+        switch (prop.getStatus()) {
+            case ADDED -> {
+                return sb.append(String.format(
+                        "Property '%s' was added with value: %s\n",
+                        prop.getName(),
+                        checkComplexValue(prop.getNewValue()))
+                );
+            }
+            case DELETED -> {
+                return sb.append(String.format("Property '%s' was removed\n", prop.getName()));
+            }
+            case UNCHANGED -> {
+                return sb;
+            }
+            case CHANGED -> {
+                return sb.append(String.format(
+                        "Property '%s' was updated. From %s to %s\n",
+                        prop.getName(),
+                        checkComplexValue(prop.getOldValue()),
+                        checkComplexValue(prop.getNewValue()))
+                );
+            }
+            default -> throw new RuntimeException();
+        }
+    }
+
+    private Object checkComplexValue(Object value) {
+        if (value instanceof String && !"null".equals(value)) {
+            return String.format("'%s'", value);
+        }
+        return value instanceof List<?> || value instanceof Map<?, ?> ? "[complex value]" : value;
+    }
+}
---
+++ app/src/test/java/hexlet/code/formatter/PlainFormatterTest.java
@@ -0,0 +1,70 @@
+package hexlet.code.formatter;
+
+import hexlet.code.property.Property;
+import hexlet.code.status.Status;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class PlainFormatterTest {
+    @Test
+    public void shouldFormatAddedProperty() {
+        var prop = Property.builder()
+                .name("key")
+                .status(Status.ADDED)
+                .newValue(Arrays.asList(1, 2, 3))
+                .build();
+        var actual = new PlainFormatter().format(new StringBuilder(), prop).toString();
+        var expected = "Property 'key' was added with value: [complex value]\n";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    public void shouldFormatDeletedProperty() {
+        var prop = Property.builder()
+                .name("key")
+                .status(Status.DELETED)
+                .oldValue("old")
+                .build();
+        var actual = new PlainFormatter().format(new StringBuilder(), prop).toString();
+        var expected = "Property 'key' was removed\n";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    public void shouldFormatUnchangedProperty() {
+        var prop = Property.builder()
+                .name("key")
+                .status(Status.UNCHANGED)
+                .oldValue("old")
+                .build();
+        var actual = new PlainFormatter().format(new StringBuilder(), prop).toString();
+        var expected = "";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    public void shouldFormatChangedProperty() {
+        var prop = Property.builder()
+                .name("key")
+                .status(Status.CHANGED)
+                .oldValue("none")
+                .newValue(true)
+                .build();
+        var actual = new PlainFormatter().format(new StringBuilder(), prop).toString();
+        var expected = "Property 'key' was updated. From 'none' to true\n";
+        assertThat(actual).isEqualTo(expected);
+    }
+    @Test
+    public void shouldFormatChangedPropertyWithNull() {
+        var prop = Property.builder()
+                .name("key")
+                .status(Status.CHANGED)
+                .oldValue("null")
+                .newValue(Map.of("k1", "v1"))
+                .build();
+        var actual = new PlainFormatter().format(new StringBuilder(), prop).toString();
+        var expected = "Property 'key' was updated. From null to [complex value]\n";
+        assertThat(actual).isEqualTo(expected);
+    }
+}
