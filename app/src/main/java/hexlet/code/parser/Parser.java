7d9e2bf5d8ad29b7d778391be42852646bdc1c5c
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-01-17 08:02:00 +0300

    Implelment YANL parser.

--- app/src/main/java/hexlet/code/parser/Parser.java
+++ app/src/main/java/hexlet/code/parser/Parser.java
@@ -2,6 +2,7 @@ package hexlet.code.parser;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +14,12 @@ public final class Parser {
     }
 
     public static Map<String, Object> parseToMap(File file) throws IOException {
+        if (file.getName().endsWith("yml")) {
+            return new YAMLMapper().readValue(file, new TypeReference<HashMap<String, Object>>() {
+            });
+        }
         return new ObjectMapper().readValue(file, new TypeReference<HashMap<String, Object>>() {
         });
     }
+
 }
