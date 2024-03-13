e9b563701fd7eb993873eaade6d12a3c5a660bab
Alexandr Starodubenko <alexvladstr@gmail.com>
2024-03-14 01:02:12 +0300

    Fix classes: FormatterFactory and FormatterFactoryTest

--- app/src/main/java/hexlet/code/formatter/FormatterFactory.java
+++ app/src/main/java/hexlet/code/formatter/FormatterFactory.java
@@ -5,12 +5,15 @@ public final class FormatterFactory {
     }
 
     public static Formatter create(String presentationFormat) {
+        if ("stylish".equals(presentationFormat)) {
+            return new StylishFormatter();
+        }
         if ("plain".equals(presentationFormat)) {
             return new PlainFormatter();
         }
         if ("json".equals(presentationFormat)) {
             return new JsonFormatter();
         }
-        return new StylishFormatter();
+        throw new IllegalArgumentException("Invalid presentationFormat: '%s'!".formatted(presentationFormat));
     }
 }
--- app/src/test/java/hexlet/code/formatter/FormatterFactoryTest.java
+++ app/src/test/java/hexlet/code/formatter/FormatterFactoryTest.java
@@ -2,7 +2,8 @@ package hexlet.code.formatter;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
 public class FormatterFactoryTest {
     @Test
@@ -15,5 +16,16 @@ public class FormatterFactoryTest {
     void shouldCreatePlainFormatter() {
         var actual = FormatterFactory.create("plain");
         assertThat(actual).isExactlyInstanceOf(PlainFormatter.class);
+    }
+
+    @Test
+    void shouldCreateJsonFormatter() {
+        var actual = FormatterFactory.create("json");
+        assertThat(actual).isExactlyInstanceOf(JsonFormatter.class);
+    }
+
+    @Test
+    void shouldThrowWhenFormatUnsupported() {
+        assertThrowsExactly(IllegalArgumentException.class, () -> FormatterFactory.create("invalid"));
     }
 }
